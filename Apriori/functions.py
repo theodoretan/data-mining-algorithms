bfa8830c3904ba5c0d72ae9c8aed7c0b7da7dd8d
Theodore <tfjtan@hotmail.com>
2016-11-28 23:21:02 -0500

    added comments

--- Apriori/functions.py
+++ Apriori/functions.py
@@ -12,12 +12,18 @@ def apriori(dataset, mins):
         itemlist - the frequent itemlist
     ------------------------------------------------------
     """
+    # _iset is the initial set of all items
+    # iset is the new set of values above the minimum support
+    # monotonicity is the list of values that are under the minimum support
+    # itemlist is the list of large item sets
     _iset, iset, monotonicity, itemlist = {}, {}, [], []
     total = 0
+    # go through the dataset and count the number of time a value is in the data
     for entry in dataset:
         for item in entry:
             _iset[item] = 1 if item not in _iset else _iset[item]+1
         total += 1 # since we're already going through the dataset we won't use len(dataset)
+    # update the set of values above the minimum support and the values under the minimum support
     for key, value in _iset.items():
         if value/total >= mins: iset[key] = value/total
         else: monotonicity.append(key)
@@ -26,8 +32,13 @@ def apriori(dataset, mins):
     itemlist = _apriori(dataset, mins, total, itemlist, monotonicity)
     return itemlist
 
+# NOTE: this is a recursive function
 def _apriori(dataset, mins, total, itemlist, monotonicity):
+    # _iset is the initial set of all items
+    # iset is the new set of values above the minimum support
     _iset, iset = {}, {}
+
+    # make sure we're still under the possible number of values
     if len(itemlist)+1 > len(itemlist[0]): return itemlist
     iterlist = list(combinations(itemlist[0], len(itemlist)+1)) # creates the k+1 combinations
 
@@ -39,6 +50,7 @@ def _apriori(dataset, mins, total, itemlist, monotonicity):
         for item in iterlist:
             if set(item).issubset(set(entry)): # checks if itemset is in the entry
                 _iset[item] = 1 if item not in _iset else _iset[item]+1
+    # check for minimum support
     for key, value in _iset.items():
         if value/total >= mins: iset[key] = value/total
         else: monotonicity.append(key)
