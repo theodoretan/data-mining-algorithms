31f2e642d1931f129263b011fd9340de8f265962
Theodore <tfjtan@hotmail.com>
2016-12-05 09:09:49 -0500

    i dont hate kmeans anymore

--- K-Means/functions.py
+++ K-Means/functions.py
@@ -1,25 +1,83 @@
+import math
 import numpy as np
+
 
 from random import randint
 
 class KMeans:
-    def __init__(self, k, dataset):
+    def __init__(self, k, dataset, x, y, t, sq_err=0.001):
         self._k = k
-        self._dataset = dataset
+        self._dataset = dataset if not t else dataset[1:]
         self._size = np.shape(dataset)
         self._centers = {}
-        self._clusters = {i: set() for i in range(k)}
+        self._clusters = {i: [[], []] for i in range(k)}
+        self._x = x
+        self._y = y
+        self._squared_error = sq_err
 
     def set_centers(self):
         for i in range(self._k):
             c = randint(0, self._size[0] - 1)
-            self._centers[i] = (self._dataset[c, 0], self._dataset[c, 1])
+            x, y = self._dataset[c, self._x], self._dataset[c, self._y]
+            self._centers[i] = (x, y)
         return
 
-    # def assign(self):
-    #     i = self._size[1] if self._size[1] % 2 == 0 else self._size[1] - 1
-    # 
-    #     for entry in self._dataset:
-    #         for index in range(0, i, 2):
-    #             x, y = entry
-    #     return
+    def assign(self):
+        while True:
+
+            for entry in self._dataset:
+                # print(entry)
+                # input()
+                x, y = entry[self._x], entry[self._y]
+
+                # compute distance and put in correct cluster
+                self._distance(x, y)
+
+            # Keep track of squared erros
+            sq_error = [False for i in range(self._k)]
+
+            # calculate the new cluster centers
+            for i in range(self._k):
+                l = len(self._clusters[i][0])
+                if l == 0:
+                    nx = self._centers[i][0]
+                    ny = self._centers[i][1]
+                else:
+                    nx = sum(self._clusters[i][0]) / l
+                    ny = sum(self._clusters[i][1]) / l
+
+                # only return if all clusters are less than the sq_err
+                if self._check_end(nx, ny, i):
+                    sq_error[i] = True
+
+                self._centers[i] = (nx, ny)
+
+            if all(sq_error):
+                return
+
+            self._clear_clusters()
+        return
+
+    def _distance(self, x, y):
+        m = math.sqrt((self._centers[0][0] - x)**2 + (self._centers[0][1] - y)**2)
+        p = 0
+        for i, j in self._centers.items():
+            d = math.sqrt((j[0] - x)**2 + (j[1] - y)**2)
+            if d < m:
+                m = d
+                p = i
+        self._clusters[p][0].append(x)
+        self._clusters[p][1].append(y)
+        return
+
+    def _check_end(self, nx, ny, i):
+        d = math.sqrt((self._centers[i][0] - nx)**2 + (self._centers[i][1] - ny)**2)
+        if d < self._squared_error:
+            return True
+        return False
+
+    def _clear_clusters(self):
+        for i in range(self._k):
+            self._clusters[i][0] = []
+            self._clusters[i][1] = []
+        return
--- K-Means/main.py
+++ K-Means/main.py
@@ -1,29 +1,64 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
+import os
 import random
 import time
 
 from argparse import ArgumentParser
 from functions import KMeans
+from matplotlib import style
 from numpy import genfromtxt
 
 parser = ArgumentParser(description="The K-Means Algorithm to Generate Clusters")
 parser.add_argument("filename", type=str, help="The dataset filename (as a .csv)")
 parser.add_argument("k", type=int, help="The number of clusters you want to generate")
+parser.add_argument("type", type=str, help="What is the datatype in the columns?", default="int")
+parser.add_argument("-x", dest="x", type=int, help="The column in the dataset for the x axis", default=0)
+parser.add_argument("-y", dest="y", type=int, help="The column in the dataset for the y axis", default=1)
+parser.add_argument("-t", dest="t", action="store_true", help="Set if there are column titles")
 parser.add_argument("-d", dest="delimiter", type=str, help="The delimiter", default=",")
 args = parser.parse_args()
 
-st = time.time()
-dataset = genfromtxt(args.filename, delimiter=args.delimiter)
+if args.type == "float":
+    t = float
+else:
+    t = int
 
-k_means = KMeans(args.k, dataset)
+st = time.time()
+dataset = genfromtxt(args.filename, delimiter=args.delimiter, dtype=t)
+print(dataset)
+
+# Test to see if it actually works
+# l = [[random.randint(0, 100), random.randint(0, 100)] for j in range(100)]
+# for j in range(50):
+#     l.append([random.randint(450,600), random.randint(300, 600)])
+# for j in range(100): l.append([random.randint(700, 1000), -random.randint(0,50)])
+# dataset = np.array(l)
+
+k_means = KMeans(args.k, dataset, args.x, args.y, args.t)
 k_means.set_centers()
-print(k_means._dataset[0:3])
-# k_means.assign()
+# print(k_means._dataset[0:3])
+k_means.assign()
+
+colors = 10*["b","g","r","c","m","y"]
+for i in range(args.k):
+    color = colors[i]
+    x_list = k_means._clusters[i][0]
+    y_list = k_means._clusters[i][1]
+
+    # for index in ra:
+    for i2 in range(np.size(x_list)):
+        plt.scatter(x_list[i2], y_list[i2], marker='+', c=color)
+
+for i in range(args.k):#k_means._centers:
+    plt.scatter(k_means._centers[i][0], k_means._centers[i][1],
+        marker='o', color='k', s=35, linewidths=5)
 
 # print(time.time() - st)
 
+os.system('espeak "K Means has found the clusters"')
 # NOTE
 # 3. assign all points to one of the Clusters
 # 4. calculate new cluster centers
 # 5. repeat 3 & 4
+plt.show()
